9e4102f8b5531af686e2099095dc0f6560a04026
תומר ספיר <sapirtomer1989@gmail.com>
2021-04-05 18:23:21 +0300

    Server setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,2 +1,16 @@
+//Import and activate express
 const express = require("express");
 const app = express();
+
+//Create a server
+const http = require("http").Server(app);
+
+//Import and connect mongoose
+const mongoose = require("mongoose");
+
+//Converting incoming and outgoing data to JSON
+app.use(express.json());
+
+//Establishing and listening to an environmental port, or a default port
+const port = process.env.PORT || 5000;
+http.listen(port, () => console.log(`Listening on port ${port}`));
