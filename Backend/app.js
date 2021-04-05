86898a1ca7c4bfec7335a2b427b46598f4da8159
תומר ספיר <sapirtomer1989@gmail.com>
2021-04-05 19:20:12 +0300

    Database setup

--- Backend/app.js
+++ Backend/app.js
@@ -5,9 +5,24 @@ const app = express();
 //Create a server
 const http = require("http").Server(app);
 
-//Import and connect mongoose
+//Import and connect mongoose, using async/await to handle the Promise
 const mongoose = require("mongoose");
-
+const config = require("./config/default.json");
+const db = config.mongoURI;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB...");
+  } catch (err) {
+    console.error(err.message);
+    //Terminates the process in order for the app to fail in case of a connection error
+    process.exit(1);
+  }
+};
+connectDB();
 //Converting incoming and outgoing data to JSON
 app.use(express.json());
 
