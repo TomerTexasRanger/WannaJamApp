14ebe6e073044ea18bc0d322a08ffcdaf33e18d2
תומר ספיר <sapirtomer1989@gmail.com>
2021-05-12 18:58:27 +0300

    deployment setting

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").Server(app);
-const connectDB = require("./config/db");
-const cors = require("cors");
+const http = require('http').Server(app);
+const connectDB = require('./config/db');
+const cors = require('cors');
+const path = require('path');
 //Routes
-const users = require("./routes/api/users");
-const auth = require("./routes/api/auth");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 connectDB();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/auth", auth);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/auth', auth);
+app.use('/api/posts', posts);
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 http.listen(port, () => console.log(`Listening on port ${port}`));
