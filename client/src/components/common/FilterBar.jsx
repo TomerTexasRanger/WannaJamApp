04971cda6068d61450436dce2bfbf2af5a128383
תומר ספיר <sapirtomer1989@gmail.com>
2021-05-14 17:05:55 +0300

    deployment bug

--- client/src/components/common/FilterBar.jsx
+++ client/src/components/common/FilterBar.jsx
@@ -22,7 +22,7 @@ const FilterBar = ({ dataArr, categories, stateFunc }) => {
     }
   };
   return (
-    <Navbar expand="lg" className="mb-3 filter-bar " fixed="none" sticky="none">
+    <Navbar expand="lg" className="mb-3 filter-bar ">
       <Navbar.Brand href="#home">Filter By:</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav ">
